e8e156102b32383771cdcc7a2b8dc70f1065c321
lukas kirchmair <kirchmair.lukas@gmail.com>
2020-10-18 19:06:09 +0200

    add fib function for js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-wasm-webworker';
+
+  isRunning = false;
+
+  jsTime = 0;
+  jsWebworkerTime = 0;
+  wasmWebworkerTime = 0;
+
+  fibonacci_js(n: number): number {
+    if (n < 0) {
+      return 0;
+    }
+    if (n < 2) {
+      return n;
+    }
+
+    return this.fibonacci_js(n - 1) + this.fibonacci_js(n - 2);
+  }
+
+  clickJsButton() {
+    this.isRunning = true;
+    setTimeout(() => {
+      const t0 = performance.now();
+      this.fibonacci_js(42).toString();
+      const t1 = performance.now();
+      this.jsTime = t1 - t0;
+      this.isRunning = false;
+    }, 50);
+  }
 }
